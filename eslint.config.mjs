8f17e66dff924ad0f95f155d25ca47b956ce0452
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-11 00:01:04 -0500

    feat(react-vite): enable lint on vite

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRedux from 'eslint-plugin-react-redux';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        globals: {
+          ...globals.browser,
+          ...globals.node,
+        },
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
+      'react-redux': reactRedux,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...reactRedux.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
     },
   },
-)
+  react.configs.flat['jsx-runtime'],
+  eslintPluginPrettierRecommended,
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
 
-function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+function app() {
+  const name = 'React';
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Test
+      </button>
+    </div>
+  );
 }
 
-export default App;
+export default app;
